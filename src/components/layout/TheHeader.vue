2c22827e2d0aa94c897838e11c68c6bf6b4d0417
Marat-Akhalov <117514563+Marat-Akhalov@users.noreply.github.com>
2024-10-08 17:59:46 +0400

    feat: add scss to the project

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <a>
+      Search Books
+    </a>
+    <RouterLink :to="{ name: 'favorites' }">Favorites</RouterLink>
+  </header>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import TheHeader from '@/components/layout/TheHeader.vue'
+
 
 </script>
 
 <template>
+  <TheHeader />
   <main>
 
   </main>
